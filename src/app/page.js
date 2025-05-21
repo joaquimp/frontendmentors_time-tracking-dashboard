ba52ea8caa0f80fb13f64a609a09c390bb2342ec
Joaquim Pessôa Filho <joaquimp@gmail.com>
2025-05-21 16:30:51 -0300

    Versão inicial

--- src/app/page.js
+++ src/app/page.js
@@ -3,93 +3,61 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <>
+      Report for
+      Jeremy Robson
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      Daily
+      Weekly
+      Monthly
+
+      Work
+      5hrs
+      Previous - 7hrs
+      32hrs
+      Previous - 36hrs
+      103hrs
+      Previous - 128hrs
+
+      Play
+      1hr
+      Previous - 2hrs
+      10hrs
+      Previous - 8hrs
+      23hrs
+      Previous - 29hrs
+
+      Study
+      0hrs
+      Previous - 1hr
+      4hrs
+      Previous - 7hrs
+      13hrs
+      Previous - 19hrs
+
+      Exercise
+      1hr
+      Previous - 1hr
+      4hrs
+      Previous - 5hrs
+      11hrs
+      Previous - 18hrs
+
+      Social
+      1hr
+      Previous - 3hrs
+      5hrs
+      Previous - 10hrs
+      21hrs
+      Previous - 23hrs
+
+      Self Care
+      0hrs
+      Previous - 1hr
+      2hrs
+      Previous - 2hrs
+      7hrs
+      Previous - 11hrs
+    </>
   );
 }
